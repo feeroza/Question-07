bf02036d9fec29a34d50dcda5643641d508eab26
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:57:45 +0500

    Add files via upload

---
+++ Question 7/007.js
@@ -0,0 +1,18 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each
+// result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+//addition
+var a = 5;
+var b = 3;
+console.log(a + b);
+//substraction
+var c = 14;
+var d = 6;
+console.log(c - d);
+//multiply
+var e = 2;
+var f = 4;
+console.log(e * f);
+//division
+var g = 24;
+var h = 3;
+console.log(g / h);
---
+++ Question 7/007.ts
@@ -0,0 +1,29 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each
+// result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+
+
+//addition
+let a: number = 5;
+let b: number = 3;
+
+console.log(a+b);
+
+
+//substraction
+let c: number = 14;
+let d: number = 6;
+console.log(c-d);
+
+
+//multiply
+let e: number = 2;
+let f: number = 4;
+console.log(e*f);
+
+
+
+//division
+let g: number = 24;
+let h: number = 3;
+console.log(g/h);
+
